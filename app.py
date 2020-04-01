2f5b36a9ba54d7c849dfbbc7cc7e425a6760ae29
Jeff Sutch <jeff.sutch@acm.org>
2020-04-01 13:56:25 -0700

    basic structure

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
 from flask import Flask
 from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Student(Resource):
+    """
+    """
+    def get(self, name):
+        """
+        get name of student
+        """
+        return {'student':name}
+
+# this replaces @app.route('xxx') under Student:get   
+api.add_resource(Student,'/student/<string:name>') #http://localhost/student/Rolf
+
+app.run(port=5000)
+
+
+
+
