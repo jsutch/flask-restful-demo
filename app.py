744563417f20fd4489b53ef0b2424d31a995b022
Jeff Sutch <jeff.sutch@acm.org>
2020-04-02 09:57:58 -0700

    added DELETE and update (PUT) functions

--- app.py
+++ app.py
@@ -29,13 +29,13 @@ class Item(Resource):
     Main Item Class
     Flask-RESTful does not need jsonify for returns
     """
-    @jwt_required()
+    #@jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None) #next() returns only the first item from filter(lambda)
         # return {'item': item}, 200 if item is not None else 404
         return {'item': item}, 200 if item else 404 #shortened version
 
-    @jwt_required()
+    #@jwt_required()
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists".format(name)}, 400
@@ -46,16 +46,34 @@ class Item(Resource):
             'price' : data['price']
             }
         items.append(item)
-        return item, 201 
-    
-    def put(self, name):
-        pass
+        return item, 201
 
+    #@jwt_required()   
     def delete(self, name):
-        pass
+        """
+        Overwrite items list with a new list that has had 'name' removed
+        """
+        global items 
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message':'Item Deleted'}
+
+    #@jwt_required()
+    def put(self, name):
+        """
+        Itempotent. Can create or update an item.
+        """
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name':name,'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item 
+
 
 class ItemList(Resource):
-    @jwt_required()
+    #@jwt_required()
     def get(self):
         return {'items': items}
 
