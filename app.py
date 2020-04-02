479df7aba788f51664ec95f5a2ad602497dd7a53
Jeff Sutch <jeff.sutch@acm.org>
2020-04-02 10:16:21 -0700

    added argparse intake for Item:PUT and Item:POST methods

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required, current_identity
 from security import authenticate, identity # implemented in security.py
 
@@ -40,7 +40,14 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists".format(name)}, 400
 
-        data = request.get_json()
+        parser = reqparse.RequestParser() #reqparser can also be used for form fields, also
+        parser.add_argument('price',
+            type=float,
+            required=True,
+            help='This field cannot be left blank'
+        )
+        data = parser.parse_args()
+
         item = {
             'name' : name,
             'price' : data['price']
@@ -62,7 +69,14 @@ class Item(Resource):
         """
         Itempotent. Can create or update an item.
         """
-        data = request.get_json()
+        parser = reqparse.RequestParser() #reqparser can also be used for form fields, also
+        parser.add_argument('price',
+            type=float,
+            required=True,
+            help='This field cannot be left blank'
+        )
+        data = parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name':name,'price': data['price']}
