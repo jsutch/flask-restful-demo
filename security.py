b052f5115682c5e83412cefe9b2d59d96304ae51
Jeff Sutch <jeff.sutch@acm.org>
2020-04-02 09:07:05 -0700

    more concise dict mapping, new User class, safe_str_cmp for password comparison

--- security.py
+++ security.py
@@ -10,24 +10,14 @@ user = [
     }
 ]
 
-username_mapping = {'bob':{
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
-
-userid_mapping = {1: {
-        'id': 1,
-        'username': 'bob',
-        'password': 'asdf'
-    }
-}
+# map dicts
+username_mapping = {u.username for u in users}
+userid_mapping = {u.id for u in users}
 
 # authenticate a user
 def authenticate(username,password):
     user = username_mapping.get(username, None) # default mapping to None if no user is found
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password, password): # safe_str_cmp safer than passwd == mypasswd for
         return user
 
 def identity(payload):
